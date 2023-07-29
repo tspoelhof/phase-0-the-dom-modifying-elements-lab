5dbb2877eaac969cb8b4feac6901cd571fbea8fe
Thomas Spoelhof <thomasjspoelhof@gmail.com>
2023-07-29 19:41:32 -0400

    dom lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainNode = document.querySelector('#main');
+mainNode.remove()
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+const thomas = 'Thomas';
+newHeader.textContent = '${thomas} is the champion';
